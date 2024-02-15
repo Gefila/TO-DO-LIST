fb1e5b8a4e3a7b96f15af1b26a67f8342b60c213
Gefila <106391467+Gefila@users.noreply.github.com>
2024-02-15 23:46:35 +0700

    fix show to do

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-let listToDo = [1,23];
-
+let listToDo = [];
 
 feather.replace();
+
+if(listToDo.length > 0){
+    showToDo()
+}
 
 function add(e) {
 	const add = document.getElementById("add");
 	const toDoInput = document.getElementById("toDoInput");
 	listToDo.push(toDoInput.value);
 	toDoInput.value = "";
-    showToDo()
+
+	showToDo();
 }
 
+function showToDo() {
+	clear();
+	for (let i = 0; i < listToDo.length; i++) {
+		const list = document.getElementById("list");
 
-function showToDo(){
-    for (let i = 0; i < listToDo.length; i++) {
-        const list = document.getElementById("list");
-    
-        const todo = document.createElement("div");
-        todo.innerHTML = `
+		const todo = document.createElement("div");
+		todo.innerHTML = `
         <input type="checkbox" name="ceklis" id="ceklis" />
         <div class="do">${listToDo[i]}</div>
         <div class="x"><i data-feather="x" class="feather"></i></div>
-        `
-        list.appendChild(todo)
-        todo.classList.add('todo')
-        feather.replace();
-    }
+        `;
+		list.appendChild(todo);
+		todo.classList.add("todo");
+		feather.replace();
+	}
+}
+
+function clear() {
+	const list = document.getElementById("list");
+
+	while (list.firstChild) {
+		list.removeChild(list.firstChild);
+	}
 }
