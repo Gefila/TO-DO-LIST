544028047fcc1ae6760259ac2ee5d282b603970a
Gefila <106391467+Gefila@users.noreply.github.com>
2024-02-16 17:30:19 +0700

    recode js

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
-let listToDo = ["sadada","dasdasd","adad"];
+let listToDo = [];
 
 feather.replace();
 
-if(listToDo.length > 0){
-    showToDo()
-}
-
 function add(e) {
-	const add = document.getElementById("add");
 	const toDoInput = document.getElementById("toDoInput");
-	listToDo.push(toDoInput.value);
-	toDoInput.value = "";
-
-	showToDo();
+	if (toDoInput.value != "") {
+		listToDo.push(toDoInput.value);
+		toDoInput.value = "";
+		showToDo();
+	} else {
+		alert("MASUKKAN TO DO");
+	}
 }
 
 function showToDo() {
-	clear();
-	for (let i = 0; i < listToDo.length; i++) {
-		const list = document.getElementById("list");
-
-		const todo = document.createElement("div");
-		todo.innerHTML = `
-        <input type="checkbox" name="ceklis" id="ceklis" />
-        <div class="do">${listToDo[i]}</div>
-        <div class="x" onclick=deletes(${[i]})><i data-feather="x" class="feather"></i></div>
-        `;
-		list.appendChild(todo);
-		todo.classList.add("todo");
-		feather.replace();
-	}
-}
-
-function clear() {
 	const list = document.getElementById("list");
 
-	while (list.firstChild) {
-		list.removeChild(list.firstChild);
-	}
+	list.innerHTML= ""
+	listToDo.forEach((value, index) => {
+		const todo = document.createElement("div");
+
+		const checkbox = document.createElement("input")
+		checkbox.type = "checkbox"
+		checkbox.checked = value.startsWith("_x_")
+		checkbox.addEventListener('change', ()=> toggle(index))
+		
+		const task = document.createElement('div')
+		task.textContent = value.startsWith("_x_") ? value.slice(3) : value
+
+		if(value.startsWith("_x_")){
+			task.classList.add('checked')
+		}
+
+		task.classList.add("do")
+
+		const remove = document.createElement('div')
+		remove.classList.add('x')
+		remove.innerHTML = '<i data-feather="x" class="feather"></i>'
+		remove.onclick = ()=>deletes(index)
+
+		todo.appendChild(checkbox)
+		todo.appendChild(task)
+		todo.appendChild(remove)
+
+		todo.classList.add("todo");
+		list.appendChild(todo);
+		feather.replace();
+	});
 }
 
-function deletes(index){
-    listToDo.splice(index,1)
+function deletes(index) {
+	listToDo.splice(index, 1);
+	showToDo();
+}
+
+function toggle(index){
+	listToDo[index] = listToDo[index].startsWith('_x_') ? listToDo[index].slice(3) : "_x_" + listToDo[index]
 	showToDo()
 }
-
-
-
